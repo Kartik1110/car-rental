9443784f7e05ec5179a70b339e974bd6cdb835b7
Kartik Pal <kartik.p1110@gmail.com>
2024-07-27 13:59:59 +0530

    feat: added nativewind, styles, fonts

--- app/index.jsx
+++ app/index.jsx
@@ -4,10 +4,10 @@ import { Link } from "expo-router";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="font-bold text-3xl">Hello!!</Text>
+    <View className="flex-1 items-center justify-center bg-primary ">
+      <Text className="font-bold text-3xl text-white mb-4">Hello User!!</Text>
       <StatusBar style="auto" />
-      <Link to="/profile" href="/profile" className="bg-indigo-700 p-2 rounded-lg text-lg text-white">
+      <Link to="/profile" href="/profile" className="bg-secondary-100 p-2 rounded-lg text-lg text-white">
         Go to Profile
       </Link>
     </View>
