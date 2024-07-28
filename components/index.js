021fde87d67afb2fde482198d9b980fd8090b0c9
Kartik Pal <kartik.p1110@gmail.com>
2024-07-29 00:09:07 +0530

    feat: added custom components

---
+++ components/index.js
@@ -0,0 +1,19 @@
+import CustomButton from "./CustomButton";
+import Loader from "./Loader";
+// import VideoCard from "./VideoCard";
+// import FormField from "./FormField";
+// import InfoBox from "./InfoBox";
+// import Trending from "./Trending";
+// import SearchInput from "./SearchInput";
+// import EmptyState from "./EmptyState";
+
+export {
+  CustomButton,
+  Loader,
+  //   VideoCard,
+  //   FormField,
+  //   InfoBox,
+  //   Trending,
+  //   SearchInput,
+  //   EmptyState,
+};
