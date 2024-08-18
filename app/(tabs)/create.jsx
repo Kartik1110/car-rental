cbb0c5295cd47534e9a14494b900290a719cd8ed
Kartik Pal <kartik.p1110@gmail.com>
2024-08-18 21:34:04 +0530

    feat: added fields for create cliet

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -60,7 +60,9 @@ const Create = () => {
         carid: selectedCar.carid,
       };
       await createClient(clientData);
-      Alert.alert("Success", "Client added successfully", [{ text: "OK", onPress: () => router.replace("/clients") }]);
+      Alert.alert("Success", "Client added successfully", [
+        { text: "OK", onPress: () => router.replace("/clients") },
+      ]);
     } catch (error) {
       Alert.alert("Error", error.message);
     } finally {
@@ -69,8 +71,8 @@ const Create = () => {
   };
 
   useEffect(() => {
-    console.log("selectedCar", selectedCar)
-  }, [selectedCar])
+    console.log("selectedCar", selectedCar);
+  }, [selectedCar]);
 
   return (
     <SafeAreaView className="bg-biege h-full p-4">
@@ -80,17 +82,34 @@ const Create = () => {
             minHeight: Dimensions.get("window").height - 150,
           }}
         >
-          <Text className="text-2xl font-semibold text-primary mt-10 font-psemibold">Add a new client</Text>
+          <Text className="text-2xl font-semibold text-primary mt-10 font-psemibold">
+            Add a new client
+          </Text>
           {[
-            { name: "fullName", title: "Full Name", placeholder: "Enter full name", keyboardType: undefined },
-            { name: "email", title: "Email", placeholder: "Enter email", keyboardType: "email-address" },
+            {
+              name: "fullName",
+              title: "Full Name",
+              placeholder: "Enter full name",
+              keyboardType: undefined,
+            },
+            {
+              name: "email",
+              title: "Email",
+              placeholder: "Enter email",
+              keyboardType: "email-address",
+            },
             {
               name: "phoneNumber",
               title: "Phone Number",
               placeholder: "Enter phone number",
               keyboardType: "phone-pad",
             },
-            { name: "address", title: "Address", placeholder: "Enter address", keyboardType: undefined },
+            {
+              name: "address",
+              title: "Address",
+              placeholder: "Enter address",
+              keyboardType: undefined,
+            },
           ].map((field) => (
             <FormField
               key={field.name}
@@ -111,10 +130,10 @@ const Create = () => {
           />
 
           <FormField
-            title="National ID"
+            title="Passport/ID"
             value={form.emiratesID}
             handleChangeText={(e) => handleChangeText("emiratesID", e)}
-            placeholder="Enter National ID"
+            placeholder="Enter Passport/ID"
             otherStyles="mt-7"
           />
 
@@ -126,30 +145,106 @@ const Create = () => {
               </View>
             </View>
           </View> */}
-          {[
-            { name: "issueDate", title: "Issue date", placeholder: "Select issue date" },
-            { name: "expiryDate", title: "Expiry date", placeholder: "Select expiry date" },
-          ].map((field) => (
-            <DatePicker
-              key={field.name}
-              title={field.title}
-              value={form[field.name]}
-              placeholder={field.placeholder}
-              handleDateChange={(e) => handleChangeText(field.name, e)}
-              otherStyles="mt-7"
-            />
-          ))}
+
+          <View className="flex flex-row justify-between">
+            {[
+              {
+                name: "issueDate",
+                title: "Issue date",
+                placeholder: "Select issue date",
+              },
+              {
+                name: "expiryDate",
+                title: "Expiry date",
+                placeholder: "Select expiry date",
+              },
+            ].map((field) => (
+              <DatePicker
+                key={field.name}
+                title={field.title}
+                value={form[field.name]}
+                placeholder={field.placeholder}
+                handleDateChange={(e) => handleChangeText(field.name, e)}
+                otherStyles="mt-7 min-w-[150px]"
+              />
+            ))}
+          </View>
+
+          <FormField
+            title="License Number"
+            // value={form.licenseNumber}
+            handleChangeText={(e) => handleChangeText("licenseNumber", e)}
+            placeholder="Enter License Number"
+            otherStyles="mt-7"
+          />
+
+          {/* <View className="mt-7">
+            <Text className="text-base mb-3 text-primary font-pmedium">Upload a photo (optional)</Text>
+            <View className="w-full h-[64px] px-4 bg-biege rounded-2xl border-2 border-black-200 flex justify-center items-center">
+              <View className="w-14 h-[40px] border border-dashed border-secondary-100 flex justify-center items-center">
+                <Image source={icons.upload} resizeMode="contain" alt="upload" className="w-1/2 h-1/2" />
+              </View>
+            </View>
+          </View> */}
+          <View className="flex flex-row justify-between">
+            {[
+              {
+                name: "issueDate",
+                title: "License Issue date",
+                placeholder: "Select issue date",
+              },
+              {
+                name: "expiryDate",
+                title: "License Expiry date",
+                placeholder: "Select expiry date",
+              },
+            ].map((field) => (
+              <DatePicker
+                key={field.name}
+                title={field.title}
+                value={form[field.name]}
+                placeholder={field.placeholder}
+                handleDateChange={(e) => handleChangeText(field.name, e)}
+                otherStyles="mt-7"
+              />
+            ))}
+          </View>
+
+          <Text className="mt-7 text-base text-primary font-pmedium text-center">Rental Information</Text>
+
           <Dropdown
             title="Select a car"
             options={carOptions}
             value={selectedCar.carname}
             handleChange={(e) => {
-              console.log(e)
-              setSelectedCar({ ...selectedCar, carname: e })
+              console.log(e);
+              setSelectedCar({ ...selectedCar, carname: e });
             }}
             otherStyles="mt-7"
           />
-          <CustomButton title="Add Client" handlePress={submit} containerStyles="mt-7" isLoading={isSubmitting} />
+
+          <DatePicker
+            title="Rental Date"
+            value={form.rentalDate}
+            placeholder="Select Rental Date"
+            handleDateChange={(e) => handleChangeText("rentalDate", e)}
+            otherStyles="mt-7"
+          />
+
+          <FormField
+            title="Rental Location"
+            // value={form.licenseNumber}
+            handleChangeText={(e) => handleChangeText("licenseNumber", e)}
+            placeholder="Enter Rental Location"
+            otherStyles="mt-7"
+          />
+
+          <CustomButton
+            title="Add Client"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
